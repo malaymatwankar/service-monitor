67707bb079c7e2318b42fb9244392e2ba1d3bede
malaymatwankar <matwankarmalay@gmail.com>
2017-11-03 15:43:06 +0530

    Removed commented dead code

--- server.js
+++ server.js
@@ -1,91 +1,3 @@
-// const request = require('request');
-// const express = require('express');
-// const app = express();
-// const server = require('http')
-//   .createServer(app);
-// const io = require('socket.io')(server);
-//
-//
-// let timeInterval = 0;
-//
-// function makeRequest(url, callback) {
-//   // console.log("This is url ", url);
-//   let options = {
-//     url: url,
-//     time: true,
-//     timeout: 5000
-//   };
-//   request.get(options, function(err, res) {
-//     if (err) {
-//       var resp = {
-//         url: url,
-//         status: '--',
-//         time: '--'
-//       }
-//       if (err.code === 'ECONNREFUSED') {
-//         resp['description'] = 'The service seems to be down please check server logs...';
-//       } else if (err.code === 'ETIMEDOUT' || err.code === 'ESOCKETTIMEDOUT') {
-//         resp['description'] = 'The service is taking too long to respond please check server logs...';
-//       } else {
-//         console.log(`Somthing bad happenned... ${err}`);
-//         resp['description'] = 'Something bad happened please check logs of service monitor as well as the relevan service...'
-//       }
-//       return callback(null, resp);
-//     } else {
-//       return callback(null, {
-//         url: url,
-//         status: res.statusCode,
-//         description: res.statusMessage,
-//         time: `${res.elapsedTime.toFixed(2)} ms`
-//       });
-//     }
-//   });
-// }
-//
-// function ping(urls) {
-//   var async = require('async');
-//   io.on('connection', function(socket) {
-//     console.log('Page loading successful...');
-//     setInterval(function asyncRequest() {
-//       async.map(urls, makeRequest, function(err, res) {
-//         if (err) {
-//           // Write socket programming code here...
-//           console.log("ERR:: ", err);
-//           socket.emit('status', err);
-//         } else {
-//           // Write socket programming code here...
-//           console.log(res);
-//           socket.emit('status', res);
-//         };
-//       });
-//       return asyncRequest;
-//     }(), 10000);
-//   });
-// }
-//
-// // function run() {
-// //   const urls = ['http://localhost:1337', 'https://yahoo.com'];
-// //   ping(urls);
-// // }
-//
-// app.set('view engine', 'ejs');
-// app.get('/', function(req, res) {
-//   const urls = ['http://localhost:1337', 'https://yahoo.com', 'http://localhost:1337', 'http://localhost:1337', 'http://localhost:1337', 'http://localhost:1337', 'http://localhost:1337', 'http://localhost:1337', 'http://localhost:1337', 'http://localhost:1337', 'http://localhost:1337', 'http://localhost:1337', 'http://localhost:1337', 'http://localhost:1337', 'http://localhost:1337', 'http://localhost:1337', 'http://localhost:1337', 'http://localhost:1337', 'http://localhost:1337', 'http://localhost:1337', 'http://localhost:1337', 'http://localhost:1337'];
-//   ping(urls);
-//   res.render('pages/index');
-// });
-// app.get('/manage-service', function(req, res) {
-//   res.render('pages/serviceManager');
-// });
-//
-// const port = 3002;
-// server.listen(port, function() {
-//   console.log(`Service Monitor Running on port ${port}`);
-// })
-
-
-
-
 const express = require('express');
 const app = express();
 const server = require('http')
@@ -93,10 +5,7 @@ const server = require('http')
 const io = require('socket.io')(server);
 const fs = require('fs');
 
-
-
 app.set('view engine', 'ejs');
-
 
 function loadControllers() {
   let controllers = fs.readdirSync(__dirname + '/src/controllers');
